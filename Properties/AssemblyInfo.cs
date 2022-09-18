ac3adc55e71a10b7a878d2518988d907bb32d3bb
Zelax <52893435+thezelax@users.noreply.github.com>
2022-09-18 12:54:52 +0300

    Create AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("Zelax Combo Editor")]
+[assembly: AssemblyDescription("Developer Edition")]
+[assembly: AssemblyCompany("Tunti Incorporate")]
+[assembly: AssemblyProduct("Combo Editor")]
+[assembly: AssemblyCopyright("ZelaxR")]
+[assembly: AssemblyTrademark("Tunti Incorporate")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
